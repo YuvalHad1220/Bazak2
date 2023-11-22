40ac444b2075f2a975a34f5719d5283229b4ac64
Yuval Hadar <35yuval2@gmail.com>
2023-11-22 23:37:03 +0200

    added more template ideas

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,6 +1,23 @@
-const Navbar = () => {
+import { ReactNode } from "react";
+import Card from "./Card"
+
+const Navbar = ({children} : {children:ReactNode}) => {
+
+    return (
+        <Card sx={{height: 78}}>
+            navbar
+        </Card>
+    )
 
 };
+
+export default Navbar;
+
+// will just include title and zminot\kshirot
+const DefaultNavbar = () => {
+
+};
+
 
 // will include column hiding, global filtering, expand to column filter
 const DataGridNavbar = () => {
